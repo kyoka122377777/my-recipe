beff0e587c0e352756fd61c49939fb543b6e9bc4
example <example@example.com>
2024-11-09 20:14:24 +0900

    Fix cache store configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_09_053518) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_09_105921) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
