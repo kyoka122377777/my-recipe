57831add5c6fe74e1211384c3b06f6fae13bb2c8
example <example@example.com>
2024-11-15 12:39:00 +0900

    home/indexに遷移１

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,15 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    #@recipes = Recipe.where(user_id: current_user.id) if current_user
+    @recipes = Recipe.where(user_id: current_user.id) if current_user
+    @recipes ||= []  # もし@recipesがnilの場合に空の配列を設定
   end
 
   private
 
   def authenticate_user!
-    # Redirect to login if the user is not authenticated
-    redirect_to login_path unless session[:user_id]
+    unless session[:user_id]
+      redirect_to login_path
+    end
   end
 end
